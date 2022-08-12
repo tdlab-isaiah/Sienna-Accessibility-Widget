bf73ac62ddf6a1a720b193fa045a106f82913d1f
Isaiah <isaiah@tdlab.io>
2022-08-12 17:33:13 -0400

    Added webpack loaders for js, and webserver for better testing

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const  path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require("webpack");
+
+module.exports = {
+    mode: 'development', 
+    entry: {
+        javascript: "./src/index.js",
+    },
+    output:{
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: '/dist'
+    },
+    module: {
+        rules: [
+          {
+            test: /\.(js)$/,
+            exclude: /node_modules/,
+            use: ['babel-loader']
+          }
+        ]
+      },
+      resolve: {
+        extensions: ['*', '.js']
+      },
+    watch: true,
+    // plugins: [new HtmlWebpackPlugin({template: '../src/index.html'})],
+    devServer: {
+        static: path.resolve(__dirname, './public'),
+        port:3000
+    }
+};
